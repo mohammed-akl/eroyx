388be8d9e2acb1a04c94626d59d0950893814f8c
muhammedashfar7 <muhammed.ashfar.7@gmail.com>
2020-06-12 16:09:03 +0300

    Update

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -29,24 +29,24 @@ jQuery(document).ready(function($){
         }
       }
     });
-	
-	
+
+
 	// toggle comments
-    $('.show-comments').on('click', function() {  
-		$('#comments').toggleClass('comments--show');		
+    $('.show-comments').on('click', function() {
+		$('#comments').toggleClass('comments--show');
 	});
 
 	//toggle search
-	$('.show-search').on('click', function() {  
-		$('.bd-search').toggleClass('search--show');		
+	$('.show-search').on('click', function() {
+		$('.bd-search').toggleClass('search--show');
 	});
-    
+
     // spoilers
      $(document).on('click', '.spoiler', function() {
         $(this).removeClass('spoiler');
      });
-    
- });   
+
+ });
 
 // deferred style loading
 var loadDeferredStyles = function () {
@@ -112,7 +112,7 @@ $(document).ready(function(){
 });
 
 $(document).keyup(function(e) {
-	if (e.keyCode == 27) { 
+	if (e.keyCode == 27) {
 		$(".nav-icon").removeClass('active');
 		$("#menu").removeClass('active');
 		$("#blackover-nav").removeClass('active');
@@ -128,7 +128,7 @@ return 'ontouchstart' in document.documentElement
 		|| navigator.msMaxTouchPoints > 0;
 }
 
-if (hasTouch()) { 
+if (hasTouch()) {
 	try {
 		for (var si in document.styleSheets) {
 			var styleSheet = document.styleSheets[si];
@@ -169,7 +169,7 @@ $(document).ready(function(){
 // DOCS
 
 $(document).ready(function(){
-    
+
 
      //Check to see if the back-menu is in the div
     $(window).scroll(function(){
